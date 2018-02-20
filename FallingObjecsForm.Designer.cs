3a34b97030ce41fbaa8b06e849e5ee7670aab8a9
Tolulope24 <Toluope.adebayo@ocsbstudent.ca>
2018-02-20 16:14:33 -0500

    Assign 2

---
+++ FallingObjecsForm.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace FallingObjectsToluA
+{
+    partial class frmFallingobjects
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.txtNumbers = new System.Windows.Forms.TextBox();
+            this.btnAnswer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuestion.Location = new System.Drawing.Point(12, 30);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(678, 29);
+            this.lblQuestion.TabIndex = 0;
+            this.lblQuestion.Text = "Enter the time (in seconds) since you dropped the object:\r\n";
+            this.lblQuestion.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(14, 118);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(451, 25);
+            this.lblAnswer.TabIndex = 1;
+            this.lblAnswer.Text = "The Height of the object above the ground is :";
+            this.lblAnswer.Click += new System.EventHandler(this.lblAnswer_Click);
+            // 
+            // txtNumbers
+            // 
+            this.txtNumbers.Location = new System.Drawing.Point(710, 37);
+            this.txtNumbers.Name = "txtNumbers";
+            this.txtNumbers.Size = new System.Drawing.Size(100, 22);
+            this.txtNumbers.TabIndex = 2;
+            this.txtNumbers.TextChanged += new System.EventHandler(this.txtNumbers_TextChanged);
+            // 
+            // btnAnswer
+            // 
+            this.btnAnswer.Location = new System.Drawing.Point(710, 87);
+            this.btnAnswer.Name = "btnAnswer";
+            this.btnAnswer.Size = new System.Drawing.Size(100, 37);
+            this.btnAnswer.TabIndex = 3;
+            this.btnAnswer.Text = "Calculate \r\n";
+            this.btnAnswer.UseVisualStyleBackColor = true;
+            this.btnAnswer.Click += new System.EventHandler(this.btnAnswer_Click);
+            // 
+            // frmFallingobjects
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(843, 205);
+            this.Controls.Add(this.btnAnswer);
+            this.Controls.Add(this.txtNumbers);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lblQuestion);
+            this.Name = "frmFallingobjects";
+            this.Text = "FallingObjects";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.TextBox txtNumbers;
+        private System.Windows.Forms.Button btnAnswer;
+    }
+}
+
---
+++ FallingObjecsForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FallingObjectsToluA
+{
+    public partial class frmFallingobjects : Form
+    {
+        public frmFallingobjects()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblAnswer_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnAnswer_Click(object sender, EventArgs e)
+        {
+            double number;
+            number = double.Parse(txtNumbers.Text);
+            lblAnswer.Text = "The Height of the object above the ground is :" + Convert.ToString(100-0.5*9.81*Math.Pow(number,2));
+        }
+
+        private void txtNumbers_TextChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+    }
+}
